acebcbc929ba4dfa7d295de1ee87bcf55ca195f0
Musab Guma'a <musabgumaa@gmail.com>
2020-06-25 16:41:00 +0100

    Updated token loading script

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+load_dotenv(join(dirname(__file__), ".env"))
+
 
 class Config:
     TOKEN = os.environ.get("mathbot-token")
