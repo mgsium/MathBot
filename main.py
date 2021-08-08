9a1cd904857ab45399558c5b4cc6bb8e37cf1395
Musab Guma'a <musabgumaa@gmail.com>
2021-08-08 23:53:53 +0100

    added results day countdown

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import random
 import os
 import requests
 import re
+from datetime import datetime, timedelta
 
 # Local Imports
 from config import Config
@@ -24,9 +25,10 @@ parodyLoader = ParodyLoader()
 # Meme Info
 new_meme_msg = ["Sending a meme!", "You asked for it...", "Meme coming up!", "Meme on the way!", "Of course!"]
 filenames = os.listdir("math_memes")
-n = 76
+n = 106
 
 # Plus Automation Article Scraping
+"""
 urls = ["https://plus.maths.org/content/Article"]
 [urls.append(f"https://plus.maths.org/content/Article?page={n}") for n in range(1, 10)]
 articleLinks = []
@@ -36,6 +38,7 @@ for URL in urls:
     soup = BeautifulSoup(response.text, "html.parser")
     articles = soup.findAll("div", {"class": "col-xs-12"})
     [articleLinks.append(f"https://plus.maths.org{article.find('a')['href']}") for article in articles]
+"""
 
 # Wolfram Alpha
 cl = wolframalpha.Client(Config.WOLFRAM_ALPHA_CLIENT_KEY)
@@ -43,11 +46,15 @@ cl = wolframalpha.Client(Config.WOLFRAM_ALPHA_CLIENT_KEY)
 # Name config
 # name_change_pattern = f"call me ([\w+])"
 
+
+
 # When Connecting to Discord...
 @client.event
 async def on_ready():
     print(f"{client.user} has connected to Discord!")
     print(client.guilds[0])
+    # channel = client.get_channel(690154591102304318)
+    # await channel.send("Formatting Fixed!!")#"So you thought I was gone....well here I am, with a brand new feature.\nType `:results day` to find out how long until you discover your fate.\nFor a special message, type `:results day helen` instead!")
 
 # When a new memeber joins...
 @client.event
@@ -121,6 +128,29 @@ async def on_message(message):
             pass
     elif "call me" in message.content.lower():
         pass
+    elif ":results day helen" in message.content.lower():
+        if (message.author.bot): return
+        now_time = datetime.now()
+        results_time = datetime.strptime("2021-08-10", "%Y-%m-%d")
+        results_time = results_time.replace(minute=30, hour=8)
+        delta = results_time-now_time
+        delta_s = delta.seconds
+        hours, remainder = divmod(delta_s, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days = delta.days
+        quote = random.choice(["https://twitter.com/JeremyClarkson/status/1293826911752392705?s=20", "https://twitter.com/JeremyClarkson/status/499876794364092416?s=20", "https://twitter.com/JeremyClarkson/status/631715075175370752?s=20", "https://twitter.com/JeremyClarkson/status/1161920575910088705?s=20", "https://twitter.com/JeremyClarkson/status/898101177040162816?s=20", "https://twitter.com/JeremyClarkson/status/766162139736371200?s=20"])
+        await message.channel.send( f"Achtung!! Only { f'{days} day ' if days > 0 else ''} {hours} hour(s), {minutes} minute(s) and {seconds} second(s) until the harrowing and ineluctable realisation of the limits of your own intelligence! But don't worry, here is a comforting word from Jeremy Clarkson: \n {quote}")
+    elif ":results day" in message.content.lower():
+        if (message.author.bot): return
+        now_time = datetime.now()
+        results_time = datetime.strptime("2021-08-10", "%Y-%m-%d")
+        results_time = results_time.replace(minute=30, hour=8)
+        delta = results_time-now_time
+        delta_s = delta.seconds
+        hours, remainder = divmod(delta_s, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days = delta.days
+        await message.channel.send( f"Achtung!! Only { f'{days} day ' if days > 0 else ''} {hours} hour(s), {minutes} minute(s) and {seconds} second(s) until results!")
 
 # Run Client
 client.run(Config.TOKEN)
